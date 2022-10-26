718e753d9f38526bc64698f7c9971617ee0c5815
Patrick Choi <ptrckchoi@gmail.com>
2022-10-26 12:23:14 -0700

    display

--- chess/display.rb
+++ chess/display.rb
@@ -7,9 +7,32 @@ class Display
     def initialize
         @cursor = Cursor.new([0,0], board)
     end
-    require "colorize"
-require_relative "cursorable"
+    def build_grid
+        @board.rows.map.with_index do |row, i|
+            build_row(row, i)
+        end
+    end
 ​
+    def build_row(row, i)
+        row.map.with_index do |piece, j|
+            color_options = colors_for(i, j)
+            piece.to_s.colorize(color_options)
+        end
+    end
+    ​
+    def colors_for(i, j)
+        if [i, j] == @cursor_pos
+            bg = :light_red
+        elsif (i + j).odd?
+            bg = :light_blue
+        else
+            bg = :blue
+        end
+        { background: bg, color: :white }
+    end
+end
+​Collapse
+
 class Display
   include Cursorable
 ​
@@ -49,21 +72,4 @@ class Display
     build_grid.each { |row| puts row.join }
   end
 end
-Collapse
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-end

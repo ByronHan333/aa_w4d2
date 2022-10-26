646768f5d03f15715f7d482df643378cbe6bea5e
Patrick Choi <ptrckchoi@gmail.com>
2022-10-26 16:47:58 -0700

    humand player

--- chess/human_player.rb
+++ chess/human_player.rb
@@ -10,13 +10,30 @@ class HumanPlayer
   end
 
   def make_move(board)
-    position = display.cursor.get_input
-    until position
+    start_position = display.cursor.get_input
+    
+    until start_position 
       puts "Enter valid input."
-      position = display.cursor.get_input
+      start_position = display.cursor.get_input
+      start_piece = board[[start_position]]
     end
-    row, col = position
+    
+    row, col = start_position
     print "#{row}, #{col} \n"
+    piece = board[[row,col]]
+
+
+
+    end_position = display.cursor.get_input
+    until end_position
+      puts "Enter valid input."
+      end_position = display.cursor.get_input
+    end
+    row, col = end_position
+    print "#{row}, #{col} \n"
+
+    
+    board.move_piece()
   end
 end
 

f4f496bd92921f57dc3f7c5faf12650d09a4ae6c
Patrick Choi <ptrckchoi@gmail.com>
2022-10-25 20:11:43 -0700

    copied king.rb to knight

--- chess/knight.rb
+++ chess/knight.rb
@@ -4,13 +4,38 @@ require_relative 'stepable'
 class Knight < Piece
     include Stepable
     
-    def symbol
-        return symbol
+    def moves
+        valid_moves
+    end
+    
+    private 
+
+    def valid_moves
+        possible_moves.select do |pos|
+            within_bound?(pos) && (empty?(pos) || opponent?(pos))
+        end
     end
 
-    private 
-    
-    def move_dirs
-        
+    def possible_moves
+        curren_pos = self.pos 
+        row, col = curren_pos
+        res = []
+        self.move_diffs.each do |dx, dy|
+            res << [row+dx, col+dy]
+        end
+        res
+    end
+
+    def move_diffs
+        [
+            [1, 2],
+            [1, -2],
+            [-1, 2],
+            [-1, -2],
+            [2, 1],
+            [2, -1],
+            [-2, 1],
+            [-2, -1]
+        ]
     end
 end
